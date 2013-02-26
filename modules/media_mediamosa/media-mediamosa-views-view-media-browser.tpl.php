d427588b55b8c279596fcdf8b70032e2cedb2bfa
Robert Blanker <rblanker@xs4all.nl>
2013-02-26 13:44:13 +0100

    By rblanker: Moved MediaMediaMosa::get_fid_from_asset() to mediamosa_ck::get_fid_from_asset(). By rblanker: The fid (fileID) is no longer set during asset search view. By rblanker: Removed function _media_mediamosa_get_media_id(), use mediamosa_ck::get_fid_from_asset() instead.

--- modules/media_mediamosa/media-mediamosa-views-view-media-browser.tpl.php
+++ modules/media_mediamosa/media-mediamosa-views-view-media-browser.tpl.php
@@ -7,17 +7,16 @@
  * @see template_preprocess_media_mediamosa_views_view_media_browser()
  * @ingroup views_templates
  */
-
 ?>
 <?php print $wrapper_prefix; ?>
-  <div id="scrollbox" class="clearfix">
+  <div class="clearfix">
     <?php print $list_type_prefix; ?>
-      <?php foreach ($rows as $id => $row) : ?>
+      <?php foreach ($rows as $id => $row): ?>
         <li id="media-item-<?php print $row->fid; ?>" class="<?php print $classes_array[$id]; ?>">
-          <?php print $row->preview;?>
+          <?php print $row->preview; ?>
         </li>
       <?php endforeach; ?>
     <?php print $list_type_suffix; ?>
     <div id="status"></div>
   </div>
-<?php print $wrapper_suffix;
+<?php print $wrapper_suffix; ?>
